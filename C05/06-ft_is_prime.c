3ab0ea343c6f16aa4a7b542e15372fb7ed7dbfce
Ludophilia <nyaritakunai@outlook.com>
2022-08-22 18:45:25 +0200

    Completed C05-06-ft_is_prime

---
+++ C05/06-ft_is_prime.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int    ft_is_prime(int nb)
+{	
+	int    i;
+	int    ht;
+
+	if (nb < 2)
+		return (0);
+	i = nb;
+	ht = 0;
+	while (i > 1)
+	{
+		if (nb % i == 0)
+			ht += 1;
+		i -= 1;
+	}
+	if (ht == 1)
+		return (1);
+	return (0);
+}
+
+int    main(void)
+{
+	int    test_nbr[] = { -1, 0, 1, 2, 10, 11, 21, 37, 59, 61, 67, 91 };
+	int    i;
+	
+	i = 0;
+	while (i < sizeof(test_nbr)/sizeof(test_nbr[0]))
+	{	
+		if (ft_is_prime(test_nbr[i]))
+			printf("%i is prime\n", test_nbr[i]);
+		i++;
+	}
+}
