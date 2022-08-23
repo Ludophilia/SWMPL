ec281d4d4a0bb3b561e219ec863c57c3db16713f
Ludophilia <nyaritakunai@outlook.com>
2022-08-23 19:30:41 +0200

    Improved C06-02-ft_rev_params

--- C06/02-ft_rev_params.c
+++ C06/02-ft_rev_params.c
@@ -3,18 +3,31 @@
 
 int    main(int argc, char **argv)
 {	
-	int    j;
-	char   c;
-
-	while(argc > 1)
+	while (argc > 1)
 	{
-		j = 0;
-		while(argv[argc - 1][j])
-		{	
-			write(1, &(argv[argc - 1][j]), 1);
-			j += 1;
+		while(*(argv[argc - 1]))
+		{
+			write(1, &(*(argv[argc - 1])), 1);
+			argv[argc - 1]++;
 		}
 		write(1, "\n", 1);
 		argc -= 1;
 	}
 }
+
+// int    main(int argc, char **argv)
+// {	
+// 	int    j;
+
+// 	while(argc > 1)
+// 	{
+// 		j = 0;
+// 		while(argv[argc - 1][j])
+// 		{	
+// 			write(1, &(argv[argc - 1][j]), 1);
+// 			j += 1;
+// 		}
+// 		write(1, "\n", 1);
+// 		argc -= 1;
+// 	}
+// }
