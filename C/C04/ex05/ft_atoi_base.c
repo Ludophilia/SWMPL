0e6521c1d84ddf9854b5752aa3906b8be56b4718
Jeffrey G <jgermany@student.42.fr>
2022-11-15 17:29:37 +0100

    Improved C04X05 ft_atoi_base and done testing it

--- C/C04/ex05/ft_atoi_base.c
+++ C/C04/ex05/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <jgermany@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 18:46:46 by jgermany          #+#    #+#             */
-/*   Updated: 2022/11/14 23:50:14 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/11/15 16:50:25 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@ int	is_sign(char c)
 	return (c == '-' || c == '+');
 }
 
-int	find_id(char c, char *base)
+int	is_space(char c)
+{
+	return ((c >= '\t' && c <= '\r') || (c == '\x20'));
+}
+
+int	id(char c, char *base)
 {
 	int	i;
 
@@ -34,15 +39,16 @@ int	check_base(char *base)
 	int		i;
 	int		j;
 
+	if (is_sign(base[0]) || is_space(base[0]))
+		return (0);
 	i = 0;
 	while (base[i])
 	{
-		if ((i == 0) && is_sign(base[i]))
-			return (0);
 		j = 1;
 		while (base[i + j])
 		{
-			if (base[i + j] == base[i] || is_sign(base[i + j]))
+			if ((base[i + j] == base[i]) || is_sign(base[i + j])
+				|| is_space(base[i + j]))
 				return (0);
 			j++;
 		}
@@ -53,18 +59,6 @@ int	check_base(char *base)
 	return (i);
 }
 
-int	is_illeg_at(int i, char *str, char *base)
-{
-	
-	if (!(((str[i] >= '\t' && str[i] <= '\r') || (str[i] == '\x20'))
-		|| is_sign(str[i]) || find_id(str[i], base) >= 0))
-		return (1);
-	if (!(is_sign(str[i + 1]) || find_id(str[i + 1], base) >= 0)
-		&& is_sign(str[i]))
-		return (1);
-	return (0);
-}
-
 int	ft_atoi_base(char *str, char *base)
 {	
 	int	i;
@@ -72,23 +66,22 @@ int	ft_atoi_base(char *str, char *base)
 	int	sign;
 	int	radix;
 
-	i = 0;
-	res = 0;
 	sign = 1;
+	res = 0;
+	i = 0;
 	radix = check_base(base);
-	if (!radix)
-		return (0);
-	while (str[i])
+	while (str[i] && (radix > 0))
 	{
-		if (!(is_space(str[i]) || is_sign(str[i]) || id(str[i], base) >= 0) ||
-			(!(is_sign(str[i + 1]) || id(str[i + 1], base) >= 0)
-			&& is_sign(str[i])))
+		if (!(is_space(str[i]) || is_sign(str[i]) || id(str[i], base) >= 0))
+			break ;
+		else if (!(is_sign(str[i + 1]) || id(str[i + 1], base) >= 0)
+			&& is_sign(str[i]))
 			break ;
 		if (str[i] == '-')
 			sign *= -1;
-		else if (find_id(str[i], base) >= 0)
-			res = (radix * res) + find_id(str[i], base);
-		if (!(find_id(str[i + 1], base) >= 0) && find_id(str[i], base) >= 0)
+		else if (id(str[i], base) >= 0)
+			res = (radix * res) + id(str[i], base);
+		if (!(id(str[i + 1], base) >= 0) && (id(str[i], base) >= 0))
 			return (sign * res);
 		i++;
 	}
