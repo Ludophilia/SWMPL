4761a15456c86e323af93b886b47cb233b3dd2e9
Jeffrey Germany <jgermany@student.42.fr>
2022-11-18 13:15:36 +0100

    Rewritten C06X03. Evil tests are coming...

--- C/C06/ex03/ft_sort_params.c
+++ C/C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <jgermany@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 22:06:33 by jgermany          #+#    #+#             */
-/*   Updated: 2022/11/17 19:17:49 by jgermany         ###   ########.fr       */
+/*   Updated: 2022/11/18 13:13:04 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,38 +27,38 @@ int	ft_strcmp(char *s1, char *s2)
 void	ft_sort_tab(char **argv)
 {
 	char	*t;
+	char	**cargv;
 
 	while (*(argv + 1))
 	{
-		while (*(*(argv + 1) + 1))
+		cargv = argv;
+		while (*(cargv + 1))
 		{
-			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			if (ft_strcmp(*cargv, *(cargv + 1)) > 0)
 			{
-				t = tab[j];
-				tab[j] = tab[j + 1];
-				tab[j + 1] = t;
+				t = *cargv;
+				*cargv = *(cargv + 1);
+				*(cargv + 1) = t;
 			}
-			**// j++;
+			cargv++;
 		}
 		argv++;
 	}
 }
 
-int	main(int argc, char s**argv)
+int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		return (1);
-	argv += 1;
 	ft_sort_tab(argv);
-	while (*argv)
+	while (*++argv)
 	{
 		while (**argv)
 		{
 			write(1, *argv, 1);
-			*argv++;
+			(*argv)++;
 		}
 		write(1, "\n", 1);
-		argv++;
 	}
 	return (0);
 }
