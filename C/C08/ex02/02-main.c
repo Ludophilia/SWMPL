0215356c027dd38b1bb0796f2d3cb294eb340b88
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2022-11-26 22:12:18 +0100

    Refreshed C08X02

--- C/C08/ex02/02-main.c
+++
@@ -1,9 +0,0 @@
-#include "02-ft_abs.h"
-#include <stdio.h>
-
-int    main(void)
-{
-	printf("ABS(-(3 * 4)) = %i\n", ABS(-(3 * 4)));
-	printf("ABS(-65356) = %i\n", ABS(-65356));
-	printf("ABS(255)) = %i\n", ABS(255));
-}
