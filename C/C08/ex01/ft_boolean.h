33df988fd13617e5d8d4f68c76146f7f863c0fc6
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2022-11-26 21:23:36 +0100

    Refreshed C08X01 and the notions it teaches.

---
+++ C/C08/ex01/ft_boolean.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 18:55:48 by jgermany          #+#    #+#             */
+/*   Updated: 2022/11/26 21:20:58 by jgermany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define SUCCESS 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef enum e_boolean
+{
+	FALSE,
+	TRUE
+}	t_bool;
+
+void	ft_putstr(char *str);
+t_bool	ft_is_even(int nbr);
+
+#endif
---
+++ C/C08/ex01/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 18:55:08 by jgermany          #+#    #+#             */
+/*   Updated: 2022/11/26 21:18:08 by jgermany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_boolean.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+t_bool	ft_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
